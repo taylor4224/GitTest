aacc09b2a5652e1d0e205e6d006e10ecae97e46b
Student <student@zpr-l0006982.local>
2017-03-20 09:09:26 -0400

    added button and label with limited constraints on the iPhone 7 that do not work on all devices. Upon the button being pressed, then the label will update with the number of times the button has pressed.

--- GitTest/ViewController.swift
+++ GitTest/ViewController.swift
@@ -9,10 +9,14 @@
 import UIKit
 
 class ViewController: UIViewController {
+    var count = 0
 
+    @IBOutlet weak var labelText: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        labelText.text = "Press the button"
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +24,14 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func buttonPressed(_ sender: Any) {
+        count += 1
+        if count == 1 {
+            labelText.text = "The button was pressed once"
+        }else {
+            labelText.text = "Pressed " + String(count) + " times."
+        }
+    }
 
 }
 
